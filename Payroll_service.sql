f9fb4f7c12da4e8d38721cda4c968748da624076
ChaurasiaP <pranshu1431@gmail.com>
2023-05-29 15:15:10 +0530

    WELCOME TO EMPLOYEE PAYROLL SERVICE

---
+++ Payroll_service.sql
@@ -0,0 +1,26 @@
+
+---------------------- UC-1 ----------------------
+
+mysql> show databases;
++---------------------+
+| Database            |
++---------------------+
+| demo                |
+| demo2               |
+| department_database |
+| information_schema  |
+| mysql               |
+| payroll_service     |
+| performance_schema  |
+| student_data        |
+| sys                 |
++---------------------+
+9 rows in set (0.01 sec)
+
+mysql> use payroll_service;
+Database changed
+mysql>
+
+------------------ END OF UC-1 ------------------
+
+

f26e844ac200274a9a90f6071e34b30f9dd7bc66
ChaurasiaP <pranshu1431@gmail.com>
2023-05-29 15:33:40 +0530

    ADDED UC2

--- Payroll_service.sql
+++ Payroll_service.sql
@@ -1,5 +1,6 @@
 
 ---------------------- UC-1 ----------------------
+------ TO CREATE A DATABASE PAYROLL_SERVICE ------
 
 mysql> show databases;
 +---------------------+
@@ -23,4 +24,37 @@ mysql>
 
 ----------------- END OF UC-1 ------------------
 
+---------------------- UC-2 ----------------------
 
+--- To create a employee payroll table in the payroll service database ---
+
+mysql> create table Employee_Payroll(
+    -> EmpID bigint(4) primary key auto_increment,
+    -> Name varchar(20),
+    -> salary bigint(10),
+    -> Start_Date date
+    -> );
+Query OK, 0 rows affected, 2 warnings (0.25 sec)
+
+mysql> show tables;
++---------------------------+
+| Tables_in_payroll_service |
++---------------------------+
+| employee_payroll          |
++---------------------------+
+1 row in set (0.00 sec)
+
+mysql> desc employee_payroll;
++------------+-------------+------+-----+---------+----------------+
+| Field      | Type        | Null | Key | Default | Extra          |
++------------+-------------+------+-----+---------+----------------+
+| EmpID      | bigint      | NO   | PRI | NULL    | auto_increment |
+| Name       | varchar(20) | YES  |     | NULL    |                |
+| salary     | bigint      | YES  |     | NULL    |                |
+| Start_Date | date        | YES  |     | NULL    |                |
++------------+-------------+------+-----+---------+----------------+
+4 rows in set (0.00 sec)
+
+mysql>
+
+----------------- END OF UC-2 ------------------
